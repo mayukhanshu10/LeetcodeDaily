dffbe8f92807cb7b1784240d80aa09f0c7a394b8
Mayukh Srivastava <51943527+mayukhanshu10@users.noreply.github.com>
2023-12-10 12:26:41 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (37.14%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -31,3 +31,11 @@ class Solution {
         getInorder(root.right,list);
     }
 }
+
+
+
+/*The time complexity of the inorder traversal is O(n), where n is the number of nodes in the binary tree. This is because, in the worst case, we may have to visit all the nodes in the tree. The recursive nature of the algorithm ensures that each node is processed once, and the work done at each node is constant.
+*/
+
+/*It's worth noting that the space complexity of the recursive inorder traversal is O(h), where h is the height of the binary tree. This is due to the recursive call stack, which can go as deep as the height of the tree. In the worst case, for a skewed tree (where each node has only one child), the height could be equal to the number of nodes, resulting in O(n) space complexity. However, for a balanced binary tree, the height is logarithmic in the number of nodes, leading to a space complexity of O(log n).
+*/
