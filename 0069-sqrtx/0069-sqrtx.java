f73618e012abc5629531d85ffaa5783e4434eef2
Mayukh Srivastava <51943527+mayukhanshu10@users.noreply.github.com>
2024-04-09 04:54:36 +0530

    Time: 1 ms (86.55%), Space: 40.8 MB (39.17%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int mySqrt(int x) {
+        long l=1,h=x;
+        long ans=0;
+        while(l<=h){
+            long m = l+(h-l)/2;
+            if(m*m<=x){
+                ans=m;
+                l=m+1;
+            }else{
+                h=m-1;
+            }
+        }
+        return (int)ans;
+    }
+}
