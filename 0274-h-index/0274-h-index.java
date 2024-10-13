b1ff7534e82efc06c310308268614cd16abc010d
Mayukh Srivastava <51943527+mayukhanshu10@users.noreply.github.com>
2024-10-13 11:46:51 +0530

    Time: 3 ms (19.8%), Space: 41.6 MB (30.62%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int hIndex(int[] citations) {
+         Arrays.sort(citations);
+        reverse(citations);
+        int h=0;
+        for(int i=0;i<citations.length;i++){
+            if((i+1)>citations[i]){
+                return h;
+            }
+            h++;
+        }
+        return h;
+    }
+
+    public static void reverse(int[] array)
+    {
+        // Length of the array
+        int n = array.length;
+
+        // Swapping the first half elements 
+        // With last Half Elements
+        for (int i = 0; i < n / 2; i++) {
+
+            // Storing the first half elements temporarily
+            int temp = array[i];
+
+            // Assigning the first half
+              // to the last half
+            array[i] = array[n - i - 1];
+
+            // Assigning the last half
+            // to the first half
+            array[n - i - 1] = temp;
+        }
+    }
+}
