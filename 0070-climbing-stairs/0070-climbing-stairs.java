df7d88f868d543779546166800760ac68add7832
Mayukh Srivastava <51943527+mayukhanshu10@users.noreply.github.com>
2024-10-16 11:16:25 +0530

    Time: 0 ms (100%), Space: 40.6 MB (19.7%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,27 @@
 class Solution {
+    // public int climbStairs(int n) {
+    //     //Tabular (From base to N)
+    //     int dp[] = new int[n+1];
+    //     dp[0]=1;
+    //     dp[1]=1;
+    //     for(int i=2;i<=n;i++){
+    //         dp[i]=dp[i-1]+dp[i-2];
+    //     }
+
+    //     return dp[n];
+    // }
+
     public int climbStairs(int n) {
         //Tabular (From base to N)
-        int dp[] = new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
+        // Space Optamization
+        int prevToPrev =1;
+        int prev =1;
+        int curr=0;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            curr = prev+prevToPrev;
+            prevToPrev=prev;
+            prev=curr;
         }
-
-        return dp[n];
+        return prev;
     }
 }
