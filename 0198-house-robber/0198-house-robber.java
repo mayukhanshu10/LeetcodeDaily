3ffb7bd79c1ce3338aa0a84327e8049274dce856
Mayukh Srivastava <51943527+mayukhanshu10@users.noreply.github.com>
2024-10-17 12:32:16 +0530

    Time: 0 ms (100%), Space: 40.8 MB (63.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,17 +1,43 @@
 class Solution {
+    //Tabular Format
+    // public int rob(int[] nums) {
+    //     int len = nums.length;
+    //     int dp[] = new int[len+1];
+    //     dp[0]= nums[0];
+    //     int profit=0;
+    //     for(int i=1;i<len;i++){
+    //         int pick = nums[i];
+    //         if(i>1){
+    //             pick += dp[i-2];
+    //         }
+    //         int notPick=dp[i-1];
+    //         dp[i]=Math.max(pick,notPick);
+    //     }
+    //     return dp[len-1];
+    // }
+
+    //Recursive Memoization
     public int rob(int[] nums) {
         int len = nums.length;
-        int dp[] = new int[len+1];
-        dp[0]= nums[0];
-        int profit=0;
-        for(int i=1;i<len;i++){
-            int pick = nums[i];
-            if(i>1){
-                pick += dp[i-2];
-            }
-            int notPick=dp[i-1];
-            dp[i]=Math.max(pick,notPick);
+        int dp[] = new int[len];
+        Arrays.fill(dp,-1);
+        return findProfit(nums,dp,len-1);
+    }
+
+    private int findProfit(int arr[], int dp[], int idx){
+        if(idx<0){
+            return 0;
         }
-        return dp[len-1];
+        if(idx == 0){
+            return arr[idx];
+        }
+
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+
+        int pick = arr[idx]+findProfit(arr, dp,idx-2);
+        int notPick = findProfit(arr, dp,idx-1);
+        return dp[idx]= Math.max(pick,notPick);
     }
 }
