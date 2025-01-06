b6dbc87b0a1ef8c2f92d3150b28dab91f4e3f482
Mayukh Srivastava <51943527+mayukhanshu10@users.noreply.github.com>
2025-01-06 13:30:47 +0530

    Time: 3 ms (94.79%), Space: 44.6 MB (77.78%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,30 +1,29 @@
 class Solution {
     public int[] minOperations(String boxes) {
-         int n = boxes.length();
-        int[] distances = new int[n];
+        int n = boxes.length();
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] res = new int[n];
 
-        int prefixCount = 0;
-        int prefixSum = 0;
-
-        for (int i = 0; i < n; ++i) {
-            distances[i] = prefixCount * i - prefixSum;
-            if (boxes.charAt(i) == '1') {
-                ++prefixCount;
-                prefixSum += i;
+        int count = (boxes.charAt(0) == '1' ? 1 : 0);
+        for (int i = 1; i < n; i++) {
+            left[i]=left[i-1]+count;
+            if(boxes.charAt(i)=='1'){
+                count++;
             }
         }
 
-        int suffixCount = 0;
-        int suffixSum = 0;
-
-        for (int i = n - 1; i >= 0; --i) {
-            distances[i] += suffixSum - suffixCount * i;
-            if (boxes.charAt(i) == '1') {
-                ++suffixCount;
-                suffixSum += i;
+        count = (boxes.charAt(n-1) == '1' ? 1 :0 );
+        for (int i = n - 2; i >= 0; --i) {
+            right[i]=right[i+1]+count;
+            if(boxes.charAt(i)=='1'){
+                count++;
             }
         }
 
-        return distances;
+        for(int i=0;i<n;i++){
+            res[i]=left[i]+right[i];
+        }
+        return res;
     }
 }
