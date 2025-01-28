d6bc27ed1b4affcef9c4366c62b1bbcd94c70f80
Mayukh Srivastava <51943527+mayukhanshu10@users.noreply.github.com>
2025-01-28 09:17:07 +0530

    Time: 0 ms (100%), Space: 42.3 MB (39.62%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,5 +1,18 @@
 class Solution {
 
+    /*
+    Intution: When we flatten 2D matrix into 1D we get [1,3,5,7,10,11,16,20,23,30,34,60]. Now we can apply
+    Binary search into it. But converting it will cause again O(m*n) tc. Therefore we will take its 
+    low and high as 0 and row*col-1 respectively. Now we will assume 1D arr (i.e. flattend array) in our mind
+    and perform Binary search, and as soon as we hit any element, the process to get that idx element value from 2D matric is
+    row = mid/col;
+    col = mid%col;  (REMEMBER THIS IS HOW WE GET IDX OF ELEMENT IN 2D MATRIX) 
+    Now just check if arr[row][col] matches target else BS on left or right half.
+
+   */
+
+
+
     public boolean searchMatrix(int[][] matrix, int target) {
         int row = matrix.length;
         int col = matrix[0].length;
