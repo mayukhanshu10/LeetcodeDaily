8cadd6c84e8744ed2bf9aa0a31b84ab34a310473
Mayukh Srivastava <51943527+mayukhanshu10@users.noreply.github.com>
2025-02-04 16:53:56 +0530

    Time: 2 ms (97.64%), Space: 41.5 MB (91.49%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,45 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
-        
-        
-        boolean flag = false;
         Stack<Character> st = new Stack<>();
-        
-        
-        for(int i =0 ; i<s.length();i++){
-            if(s.charAt(i) == '[' || s.charAt(i) == '{' || s.charAt(i) == '(' ){
-                flag = true;
-                st.push(s.charAt(i));
-            }else{
-                if(st.size()>0){ // Might throw null pointer exception if doesnt put this check
-                    char x = st.peek(); 
-                    if(s.charAt(i)==')'){
-                        if(x=='('){
-                            st.pop();
-                        }else{
-                            return false;
-                        }
-                    }else if(s.charAt(i)=='}'){
-                        if(x=='{'){
-                            st.pop();
-                        }else{
-                            return false;
-                        }
-                    }else if(s.charAt(i)==']'){
-                        if(x=='['){
-                            st.pop();
-                        }else{
-                            return false;
-                        }
-                    }
-                }else{return false;}
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.isEmpty()) return false; // Early exit if no matching opening bracket
+
+                char top = st.pop();
+                if ((c == ')' && top != '(') || 
+                    (c == '}' && top != '{') || 
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
             }
         }
-        if(st.isEmpty() && flag == true){
-                return true;
-            }
-            return false;
-        
-        
+
+        return st.isEmpty();
     }
 }
